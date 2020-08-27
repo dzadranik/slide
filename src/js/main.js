3d1557266600e0a35d72c7762a8b166890b5c0b2
Victoria <dzadranik@gmail.com>
2020-08-27 09:41:20 +0300

    slide move fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,71 @@
 import '../sass/main.sass'
 
-let wait = document.querySelector('.wait'),
-    marker = true,
-    direction,
-    interval = 150,
-    counter1 = 0,
-    counter2
+const wait = document.querySelector('.wait'),
+    sliders = document.querySelector('.sliders'),
+    move = moveElements()
+let slider = {
+    countElements: document.querySelectorAll('.sliders__slide').length - 1,
+    intervalWheel: 70,
+    intervalSlider: 600,
+    marker: true,
+    counter1: 0,
+    counter2: false,
+}
 
 function wheel(e) {
-    counter1 += 1
+    let direction
+    slider.counter1 += 1
     e.deltaY > 0 ? (direction = 'up') : (direction = 'down')
-    if (marker) {
-        wheelAct()
+    if (slider.marker) {
+        wheelAct(direction)
     }
     return false
 }
-function wheelAct() {
-    if (marker) moveElements(direction)
 
-    marker = false
-    counter2 = counter1
+// блокирует слайдер если wheel генерируется безостановочно
+function wheelAct(direction) {
+    if (slider.marker) move(direction)
+
+    slider.marker = false
+    slider.counter2 = slider.counter1
     setTimeout(function () {
-        if (counter2 == counter1) {
+        if (slider.counter2 == slider.counter1) {
             wait.classList.remove('is-show')
-            marker = true
-            counter1 = 0
-            counter2 = false
+            slider.marker = true
+            slider.counter1 = 0
+            slider.counter2 = false
         } else {
             wheelAct()
         }
-    }, interval)
+    }, slider.intervalWheel)
 }
 
-function moveElements(direction) {
-    const element = document.querySelector('.sliders__slide.is-active')
-    if (direction === 'up' && element.nextSibling) {
-        element.classList.remove('is-active')
-        element.nextSibling.classList.add('is-active')
-        wait.classList.add('is-show')
-    } else if (direction === 'down' && element.previousSibling) {
-        element.classList.remove('is-active')
-        element.previousSibling.classList.add('is-active')
-        wait.classList.add('is-show')
+// передвигает элементы с минимальной частотой slider.intervalSlider
+function moveElements() {
+    let timer = true,
+        top = 0,
+        activeElement = 0
+
+    return function (direction) {
+        if (timer) {
+            timer = false
+            if (direction === 'up' && activeElement < slider.countElements) {
+                activeElement += 1
+            } else if (direction === 'down' && activeElement > 0) {
+                activeElement -= 1
+            }
+            let coord = document
+                .querySelector(`[data-number="${activeElement}"]`)
+                .getBoundingClientRect()
+
+            top = coord.top + top
+            wait.classList.add('is-show')
+            sliders.style.top = `-${top}px`
+
+            setTimeout(() => {
+                timer = true
+            }, slider.intervalSlider)
+        }
     }
 }
 
